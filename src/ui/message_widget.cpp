11668ef9501bfa07a157e2e6633cd0e546eb3dd2
Alexey Gostyunin <bortone52@yandex.ru>
2026-09-17 16:01:36 +0300

    Change .gitignore

---
+++ src/ui/message_widget.cpp
@@ -0,0 +1,6 @@
+#include "message_widget.hpp"
+
+message_widget::message_widget()
+{
+
+}
---
+++ src/ui/message_widget.hpp
@@ -0,0 +1,11 @@
+#ifndef MESSAGE_WIDGET_H
+#define MESSAGE_WIDGET_H
+
+
+class message_widget
+{
+public:
+    message_widget();
+};
+
+#endif // MESSAGE_WIDGET_H
